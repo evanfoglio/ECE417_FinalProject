0eef5e4547044b98ee55748f97bed8f3cee0b332
Evan Foglio <evan.foglio@maine.edu>
2022-04-13 18:09:36 -0400

    Added subscriber, commiting to test on Ubuntu

--- detect_checkers.py
+++ detect_checkers.py
@@ -1,3 +1,29 @@
 #!/usr/bin/python3
-
+import rospy
 import cv2
+from std_msgs.msg import String
+
+
+def callback(data):
+    #once we have the image in here, we can error check to see if theres a checker board or not
+
+    rospy.loginfo(rospy.get_caller_id() + "Oh boy, Garbage: ", str(data))
+    
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber("/camera/image_raw", String, callback)
+
+    rospy.spin()
+
+
+if '__name__' == '__main__':
+    listener()
+
+
+
+
+
+
+
