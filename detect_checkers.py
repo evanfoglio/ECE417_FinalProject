90ec476c8b4c4b4e4bb1234868f9b05ae85412ae
Evan Foglio <evan.foglio@maine.edu>
2022-04-14 10:39:39 -0400

    Added comments referencing documentation and examples for future reference

--- detect_checkers.py
+++ detect_checkers.py
@@ -31,8 +31,12 @@ def callback(data):
     #show image
     plt.imshow(img, interpolation='nearest')
     plt.show()
-
-    #time.sleep(5)
+    
+    # example checkerboard detecton line:
+    # Find the chess board corners
+    # ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    # Full code example: https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
+    # Documentation on the function: https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html#ga93efa9b0aa890de240ca32b11253dd4a
 
 def listener():
     rospy.init_node('listener', anonymous=True)
