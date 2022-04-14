f9718d87a231b5744b4aeafaf8a5281b6a4f22ae
Evan Foglio <evan.foglio@maine.edu>
2022-04-14 16:24:05 -0400

    Now detects the corners of checkerboard and plots them on top of the checkerboard image

--- detect_checkers.py
+++ detect_checkers.py
@@ -9,36 +9,49 @@ import time
 from matplotlib import pyplot as plt
 from PIL import Image as im
 
-
+#This function is called everytime an image
+#is published to the "camera/image_raw/" topic
 def callback(data):
-    #once we have the image in here, we can error check to see if theres a checker board or not
 
-    #rospy.loginfo(rospy.get_caller_id() + " This will display to terminal ")
-
-    #print("type of original data: " + str(type(data)))
     bridge = CvBridge()
     img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
     
-    #debug print statements
-    #print("Type of img: " + str(type(img)))
-    #print("img shape: " + str(img.shape)) 
-   
+    #Attempt to find checkerboard corners, ret = 1 if successful
     ret, corners = cv2.findChessboardCorners(img, (6,6), None)
-    # If a checker board is detected, image is displayed 
+    # If a checker board is detected, 
     if ret == True:
+        #define arrays to hold xy coordinates
+        x = []
+        y = []
+        
         print("Found checkers")
+        
+        #plot the image
         plt.imshow(img, interpolation='nearest')
+
+        # pull the individual coordinates from the 
+        # return value of findChessboard
+        for xy in corners:
+            x.append(xy[0][0])
+            y.append(xy[0][1])
+        #plot coordinates on top of image
+        plt.scatter(x, y, color='red')
+        #display image
         plt.show()
-    # example checkerboard detecton line:
-    # Find the chess board corners
-    # ret, corners = cv.findChessboardCorners(gray, (7,6), None)
-    # Full code example: https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
-    # Documentation on the function: https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html#ga93efa9b0aa890de240ca32b11253dd4a
+
+
+
+# example checkerboard detecton line:
+# Find the chess board corners
+# ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+# Full code example: https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
+# Documentation on the function: https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html#ga93efa9b0aa890de240ca32b11253dd4a
 
 def listener():
     rospy.init_node('listener', anonymous=True)
     #subscribe to image node
     rospy.Subscriber("/camera/image_raw", Image, callback)
+    #wait for callbacks
     rospy.spin()
 
 if __name__ == '__main__':
