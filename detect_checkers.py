f23c351a134f915e79d6ed25c8fcd2a108617ba3
Evan Foglio <evan.foglio@maine.edu>
2022-04-14 10:27:07 -0400

    Now receives images from the ROS image topic and displays them 1 fram at a time.

--- detect_checkers.py
+++ detect_checkers.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python3
 import rospy
+import numpy as np
 import cv2
 from std_msgs.msg import String
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+import time
+from matplotlib import pyplot as plt
+
 
 
 def callback(data):
     #once we have the image in here, we can error check to see if theres a checker board or not
 
-    rospy.loginfo(rospy.get_caller_id() + "Oh boy, Garbage: ", str(data))
+    #rospy.loginfo(rospy.get_caller_id() + " This will display to terminal ")
+
+    print("type of original data: " + str(type(data)))
+    bridge = CvBridge()
+    img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
     
+    #debug print statements
+    print("Type of img: " + str(type(img)))
+    print("img shape: " + str(img.shape)) 
+   
+
+    #Currently displays 1 frame at a time and
+    # waits for the previous frome to be closed 
+    # before it grabs the next frame
+
+    #show image
+    plt.imshow(img, interpolation='nearest')
+    plt.show()
+
+    #time.sleep(5)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber("/camera/image_raw", String, callback)
-
+    #subscribe to image node
+    rospy.Subscriber("/camera/image_raw", Image, callback)
     rospy.spin()
 
-
-if '__name__' == '__main__':
+if __name__ == '__main__':
+    
     listener()
 
 
