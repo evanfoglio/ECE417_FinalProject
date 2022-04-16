1ea9b7a8f69d5200ea05da545798313f631ec4bf
Evan Foglio <evan.foglio@maine.edu>
2022-04-16 19:03:58 -0400

    Removed threading
    now detects 2 checkerboards and shows image with all points plotted

--- detect_checkers.py
+++ detect_checkers.py
@@ -10,40 +10,57 @@ from matplotlib import pyplot as plt
 from PIL import Image as im
 from threading import Thread
 
+
 #This function is called everytime an image
 #is published to the "camera/image_raw/" topic
 def callback(data):
-    img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-    
-    #Attempt to find checkerboard corners, ret = 1 if successful
-    ret, corners = cv2.findChessboardCorners(img, (6,6), None)
-    # If a checker board is detected, 
-    if ret == True:
-        #define arrays to hold xy coordinates
-        #x = []
-        #y = []
-        
-        print("Found checkers")
-        
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        thread1 = Thread(target=show_img, args=(img, corners))
-        thread1.start()
-        val = input("Enter X coordinate of y axis:")
-        print(val)
+    global i
+    if(i == 20):
+        i = 1
+        print("Checking...")
+        img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+        #Attempt to find checkerboard corners, ret = 1 if successful
+        ret, corners = cv2.findChessboardCorners(img, (7,7), None)
+        # If a checker board is detected, 
+        if ret == True:
+            print("Found First Checkerboard")
+            start_point = corners[0][0]
+            end_point = corners[-1][0] 
+            start_point = [int(start_point[0]), int(start_point[1])]
+            end_point = [int(end_point[0]), int(end_point[1])]
             
-        while True:
-            dummy_var = 0
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img_copy = img.copy()
+            color = [0, 0, 0]
+            
+            img2 = cv2.rectangle(img, start_point, end_point, color, -1)
 
-def show_img(img, corners):
+            ret, corners2 = cv2.findChessboardCorners( img2, (7,7), None)
+
+            if ret == True:
+                print("Found Second Checkerboard")
+                show_img(img_copy, corners, corners2)
+            else:
+                print("Second Board Not Found")
+
+    else:
+        i = i + 1
+
+def show_img(img, corners, corners2):
     x = []
     y = []
+    x1 = []
+    y1 = []
     plt.imshow(img, interpolation='nearest')
     for xy in corners:
         x.append(xy[0][0])
         y.append(xy[0][1])
+    for xy in corners2:
+        x1.append(xy[0][0])
+        y1.append(xy[0][1])
     #plot coordinates on top of image
     plt.scatter(x, y, color='blue')
-
+    plt.scatter(x1, y1, color='green')
     plt.show()
 
 
@@ -60,8 +77,9 @@ def listener():
     rospy.Subscriber("/camera/image_raw", Image, callback)
     #wait for callbacks
     rospy.spin()
-
-bridge = CvBridge()    
+i = 1
+bridge = CvBridge() 
+print("Bout to listen")
 listener()
 
 
